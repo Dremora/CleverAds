6904382726de35c0cbc6c59fb97d88e1ad990699
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-06-06 23:56:10 +0100

    Add /presentation-video route

---
+++ tests/unit/presentation-video/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:presentation-video', 'Unit | Route | presentation video', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
