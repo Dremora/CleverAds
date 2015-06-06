450c7148efb2ef42c867017d718e51f5e32e4bfe
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-06-06 15:02:37 +0100

    Moved html snippets out of js

--- js/main.js
+++ js/main.js
@@ -38,62 +38,42 @@ window.dmAsyncInit = function()
     console.log("playing");
   });
 
-  //----- PLAY / PAUSE - OK -----//
-  var controls = ['pause', 'play'];
-  for (var i = 0; i < controls.length; i++)
-  {
-    $('#controls').append('<button id=\'' + controls[i] + '\'>' + controls[i] + '</button>');
-    var lc = '#' + controls[i];
-    $(lc).click(function() {
-
-      eval('player.' + this.id + '();');
-    });
-  }
-
   //----- TOGGLE PLAY - OK to play, NOK to pause -----//
-  $('#controls').append('<button id="togglePlay">togglePlay</button>');
   $('#togglePlay').click(function() {
     player.togglePlay();
   });
 
   //----- SEEK - OK -----//
-  $('#controls').append('<button id="seek">seek</button>');
   $('#seek').click(function() {
     player.seek(10);
   });
 
   //----- LOAD - OK -----//
-  $('#controls').append('<button id="load">load</button>');
   $('#load').click(function() {
     player.load("x174uig");
   });
 
   //----- SETVOLUME - OK -----//
-  $('#controls').append('<button id="setVolume">setVolume</button>');
   $('#setVolume').click(function() {
     player.setVolume(0.2);
   });
 
   //----- UNMUTE - NOK -----//
-  $('#controls').append('<button id="unmute">unmute</button>');
   $('#unmute').click(function() {
     player.setMuted(0);
   });
 
   //----- MUTE - OK -----//
-  $('#controls').append('<button id="mute">mute</button>');
   $('#mute').click(function() {
     player.setMuted(1);
   });
 
   //----- TOGGLEMUTE - OK -----//
-  $('#controls').append('<button id="toggleMute">toggleMute</button>');
   $('#toggleMute').click(function() {
     player.toggleMuted();
   });
 
   //----- FULLSCREEN - NOK -----//
-  $('#controls').append('<button id="setFullscreen">setFullscreen</button>');
   $('#setFullscreen').click(function() {
     player.setFullscreen(1);
   });
