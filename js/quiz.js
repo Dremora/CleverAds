29e7a98c1530eba5f372f2219c7d956ff1c97cf9
Cornel Andreev <hugronaphor@gmail.com>
2015-06-06 16:46:16 +0100

    Quiz concept implementation.

---
+++ js/quiz.js
@@ -0,0 +1,29 @@
+(function() {
+
+  var getTheQuiz = function(quizName) {
+
+    $.get("/tpl/quiz/" + quizName + ".json", function(data) {
+
+      var output = "";
+      var quizTitle = data.title;
+
+      for (var i = 0; i < 1; i++) {
+
+        var question = data.questions[i];
+        var questionTitle = question.title;
+
+        output += '<label>' + questionTitle + '</label>';
+        $.each(question.options, function(index, value) {
+          output += '<button data="' + index + '">' + value + '</button>';
+        });
+      }
+
+      $('#the-quiz').html(output).removeClass('hidden');
+    });
+  };
+
+  getTheQuiz('1');
+
+}());
+
+
