cd66208db63ae05908a56f8e417fec8d05b0a7fe
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-06-06 16:27:39 +0100

    Ad switching

--- js/main.js
+++ js/main.js
@@ -1,48 +1,71 @@
-function createVideoPlayer() {
-  // PARAMS is a javascript object containing parameters to pass to the player if any (eg: {autoplay: 1})
-  var PARAMS = {background: 'ABE866', autoplay: 1, chromeless: 0,
-    foreground: '000000',
-    html: 0, highlight: '857580',
-    info: 1, network: 'dsl', autoplay : 0};
-  return DM.player("player", {
-    video: "x2sx8z4",
-    width: "640",
-    height: "360",
-    params: PARAMS
+$(function () {
+  function createVideoPlayer() {
+    // PARAMS is a javascript object containing parameters to pass to the player if any (eg: {autoplay: 1})
+    var PARAMS = {background: 'ABE866', autoplay: 1, chromeless: 0,
+      foreground: '000000',
+      html: 0, highlight: '857580',
+      info: 1, network: 'dsl', autoplay : 0};
+    return DM.player("player", {
+      video: "x2sx8z4",
+      width: "640",
+      height: "360",
+      params: PARAMS
+    }
+    );
   }
-  );
-}
 
-function createAdPlayer() {
-  // PARAMS is a javascript object containing parameters to pass to the player if any (eg: {autoplay: 1})
-  var PARAMS = {background: 'ABE866', autoplay: 1, chromeless: 0,
-    foreground: '000000',
-    html: 0, highlight: '857580',
-    info: 1, network: 'dsl', autoplay : 0, chromeless: true};
-  return DM.player("ad", {
-    video: "x2sx8z4",
-    width: "640",
-    height: "360",
-    params: PARAMS
+  function createAdPlayer() {
+    // PARAMS is a javascript object containing parameters to pass to the player if any (eg: {autoplay: 1})
+    var PARAMS = {background: 'ABE866', autoplay: 1, chromeless: 0,
+      foreground: '000000',
+      html: 0, highlight: '857580',
+      info: 1, network: 'dsl', autoplay : 0, chromeless: true};
+    return DM.player("ad", {
+      video: "xx6hd9",
+      width: "640",
+      height: "360",
+      params: PARAMS
+    }
+    );
   }
-  );
-}
 
-// This function init the player once the SDK is loaded
-$(function()
-{
+  function showAd() {
+    $('#ad').addClass('active');
+    $('#player').removeClass('active');
+  }
+
+  function showPlayer() {
+    $('#player').addClass('active');
+    $('#ad').removeClass('active');
+  }
+
+
   var player = createVideoPlayer();
   var ad = createAdPlayer();
 
-  // 4. We can attach some events on the player (using standard DOM events)
-  player.addEventListener("apiready", function(e)
-  {
-    console.log('apiready');
+
+  ad.addEventListener('apiready', function () {
+    ad.play();
+    $(ad).one('playing', function () {
+      ad.removeEventListener('playing');
+      ad.pause();
+
+      ad.addEventListener('ended', function () {
+        player.play();
+      })
+    });
   });
 
-  player.addEventListener("playing", function(e)
-  {
-    console.log("playing");
+
+  var prevTime = 0;
+  player.addEventListener('timeupdate', function (event) {
+    var currentTime = event.srcElement.currentTime;
+    if (prevTime < 5 && currentTime > 5) {
+      player.pause();
+      showAd();
+      ad.play();
+    }
+    prevTime = currentTime;
   });
 
   //----- TOGGLE PLAY - OK to play, NOK to pause -----//
