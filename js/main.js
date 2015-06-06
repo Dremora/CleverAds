65b01bfea919b4f19c97dfc687a66e5747b87fcf
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-06-06 15:25:27 +0100

    Add ad player

--- js/main.js
+++ js/main.js
@@ -1,31 +1,38 @@
-// This code loads the Dailymotion Javascript SDK asynchronously.
-(function() {
-
-  var e = document.createElement('script');
-  e.async = true;
-  e.type = 'text/javascript';
-  e.src = document.location.protocol + '//api.dmcdn.net/all.js';
-  var s = document.getElementsByTagName('script')[0];
-
-  s.parentNode.insertBefore(e, s);
-}());
-
-// This function init the player once the SDK is loaded
-window.dmAsyncInit = function()
-{
+function createVideoPlayer() {
   // PARAMS is a javascript object containing parameters to pass to the player if any (eg: {autoplay: 1})
   var PARAMS = {background: 'ABE866', autoplay: 1, chromeless: 0,
     foreground: '000000',
     html: 0, highlight: '857580',
     info: 1, network: 'dsl', autoplay : 0};
-  var player = DM.player("player", {
+  return DM.player("player", {
     video: "x2sx8z4",
     width: "640",
     height: "360",
     params: PARAMS
   }
   );
+}
 
+function createAdPlayer() {
+  // PARAMS is a javascript object containing parameters to pass to the player if any (eg: {autoplay: 1})
+  var PARAMS = {background: 'ABE866', autoplay: 1, chromeless: 0,
+    foreground: '000000',
+    html: 0, highlight: '857580',
+    info: 1, network: 'dsl', autoplay : 0, chromeless: true};
+  return DM.player("ad", {
+    video: "x2sx8z4",
+    width: "640",
+    height: "360",
+    params: PARAMS
+  }
+  );
+}
+
+// This function init the player once the SDK is loaded
+$(function()
+{
+  var player = createVideoPlayer();
+  var ad = createAdPlayer();
 
   // 4. We can attach some events on the player (using standard DOM events)
   player.addEventListener("apiready", function(e)
@@ -77,4 +84,4 @@ window.dmAsyncInit = function()
   $('#setFullscreen').click(function() {
     player.setFullscreen(1);
   });
-}
+});
