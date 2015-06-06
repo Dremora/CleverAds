cbcf098aa06a615e830591a0890c38161f0cfbc0
Kirill Korolyov <kirill.korolyov@gmail.com>
2015-06-06 23:50:54 +0100

    Proper questions

--- app/application/route.js
+++ app/application/route.js
@@ -3,26 +3,30 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return [{
-      "title": "How are you?",
-      "correctAnswer": 2,
+      "title": "What are the creatures always fighting for?",
+      "correctAnswer": 3,
+      "correctAnswerMessage": "Well done!",
+      "incorrectAnswerMessage": "Come on, you can do better",
       "options": [
         { "id": 1,
-          "title": "Answer 1" },
+          "title": "Potato" },
         { "id": 2,
-          "title": "Answer 2" },
+          "title": "Rabbit" },
         { "id": 3,
-          "title": "Answer 3" },
+          "title": "Banana" },
       ]
     }, {
-      "title": "Are you kidding me?",
+      "title": "Do you at least know the name of the film?",
       "correctAnswer": 2,
+      "correctAnswerMessage": "We're getting you back in just a second!",
+      "incorrectAnswerMessage": "Enjoy the ad!",
       "options": [
         { "id": 1,
-          "title": "Answer 1" },
+          "title": "Minions" },
         { "id": 2,
-          "title": "Answer 2" },
+          "title": "Despicable Me" },
         { "id": 3,
-          "title": "Answer 3" },
+          "title": "Skyfall" },
       ]
     }];
   }
